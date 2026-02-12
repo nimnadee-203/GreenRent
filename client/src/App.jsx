dd089c4cb4ec04f3fb8e867c4258ad7fde7cc335
Yasanga32 <yasangashanuka33@gmail.com>
2026-02-12 20:09:32 +0530

    Fix Vite import issues and update App and index

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,13 @@
-import './App.css';
-import { Route,Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home';
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Home/>}/>
+        <Route path='/' element={<Home />} />
       </Routes>
-      
+
     </div>
   );
 }
